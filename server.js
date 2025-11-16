c9af77e983a777944e9418fb51fbe1fa1eda3f41
kapil <devops_lab@Kapils-MacBook-Air.local>
2025-11-16 17:20:35 +0530

    first commit to repo

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello from Node.js! Your server is running 🎉\n");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is live at http://localhost:${PORT}`);
+});
